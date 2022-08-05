e5555778e3e645a6a9551a6451238e4a03774e07
sachinit254 <sachinit254@gmail.com>
2022-08-05 15:08:05 +0530

    User, Comments, Video CRUD api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,92 @@
-export const test = (req, res) => {
-    res.json("Its successfull")
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+
+export const updateUser = async (req, res, next) => { };
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You can delete only your account!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.status(200).json(user)
+  } catch (error) {
+    next(error)
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users)
+  } catch (error) {
+    next(error)
+  }
 }
+
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subscribedUsers: req.params.id }
+    })
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 }
+    })
+    res.status(200).json("Subscription successfull.")
+  } catch (error) {
+    next(error)
+  }
+};
+export const unsubscribe = async (req, res, next) => {
+  try {
+    try {
+      await User.findByIdAndUpdate(req.user.id, {
+        $pull: { subscribedUsers: req.params.id }
+      })
+      await User.findByIdAndUpdate(req.params.id, {
+        $inc: { subscribers: -1 }
+      })
+      res.status(200).json("Unsubscription successfull.")
+    } catch (error) {
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+};
+export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+  try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id }
+    })
+    res.status(200).json("The video has been liked!")
+  } catch (error) {
+    next(error)
+  }
+};
+export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+  try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id }
+    })
+    res.status(200).json("The video has been disliked!")
+  } catch (error) {
+    next(error)
+  }
+};
